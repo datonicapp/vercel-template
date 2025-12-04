92338ef5f8833525e0b297bb9c6b78a3e6e5a813
datonicapp <admin@datonic.app>
2025-12-04 16:57:18 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Vercel Template Works</h1>
+      <p>This is the stable base template.</p>
+    </main>
+  );
+}
